66e18d65f9ac615a5c90cad07c4c8ba58bfd99ae
6okuJins <d.ma@mail.utoronto.ca>
2022-07-22 00:28:30 -0400

    Fixed logic in placeShip method

--- src/classes/Gameboard.js
+++ src/classes/Gameboard.js
@@ -1,8 +1,14 @@
 const Gameboard = () => {
-  const board = Array.apply(null, new Array(100));
+  const board = {};
   const placeShip = (ship, coordinates) => {
-    for (let i = 0; i < coordinates.length; i++) {
-      board[coordinates[i]] = ship;
+    // check if ship is already placed in that spot
+    // REMEMBER: if using curly brace in arrow function, use return keyword. If omit brace, omit return.
+    // REMEMBER:  Object.keys returns array of keys in string format, even if they're supposed to be something else.
+    if (coordinates.some((x) => {return Object.keys(board).includes(String(x))})) {
+      return
+    }
+    for (const key of coordinates) {
+      board[key] = ship;
     }
   }
   const getBoard = () => {
