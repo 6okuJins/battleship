989bfe69f1a3ea0b3cf089310714fd2a8cfdbfed
6okuJins <d.ma@mail.utoronto.ca>
2022-07-22 19:16:13 -0400

    Update Gameboard factory

--- src/classes/Gameboard.js
+++ src/classes/Gameboard.js
@@ -1,5 +1,12 @@
 const Gameboard = () => {
   const board = {};
+  let sunkenShips = 0;
+  const getBoard = () => {
+    return board;
+  }
+  const getSunkenShips = () => {
+    return sunkenShips;
+  }
   const placeShip = (ship, coordinates) => {
     // check if ship is already placed in that spot
     // REMEMBER: if using curly brace in arrow function, use return keyword. If omit brace, omit return.
@@ -11,12 +18,31 @@ const Gameboard = () => {
       board[key] = ship;
     }
   }
-  const getBoard = () => {
-    return board;
+  const recieveAttack = (coordinates) => {
+    const ship = board[coordinates];
+    if (ship) {
+      // not sure if I should use ship methods here since not importing ship module
+      if (!ship.getHitPositions().includes(coordinates)) {
+        ship.hit(coordinates);
+        if (ship.isSunk()) {
+          sunkenShips ++;
+        }
+      }
+    }
+    else {
+      board[coordinates] = ship;
+    }
   }
+  const gameOver = () => {
+    return (sunkenShips === 5);
+  }
+
   return {
     placeShip,
-    getBoard
+    getBoard,
+    getSunkenShips,
+    recieveAttack,
+    gameOver
   }
 }
 export default Gameboard;
