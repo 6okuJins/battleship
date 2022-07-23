67016adb598f3859dea18f4996304f3850e848f0
6okuJins <d.ma@mail.utoronto.ca>
2022-07-22 21:01:05 -0400

    Update Ai class

--- src/classes/Ai.js
+++ src/classes/Ai.js
@@ -1,6 +1,32 @@
-import Player from '.';
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
 
 const Ai = () => {
-  const {setVisited, getVisited } = Player();
+  const moves = shuffle(new Array(100).fill().map((_, index) => index));
+  const getMoves = () => {
+    return moves.slice();
+  }
+  const getMove = () => {
+    return moves.pop();
+  }
+  return {
+    getMoves,
+    getMove
+  }
 }
 export default Ai;
