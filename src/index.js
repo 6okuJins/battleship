c4096e7c5f5c4279fd7d17e9e6559e7298621e91
6okuJins <d.ma@mail.utoronto.ca>
2022-07-26 21:26:18 -0400

    Tie game logic to UI

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
 import { Controller } from './classes';
-import { main } from './views';
+import { Main } from './views';
 import { meyerReset } from './styles';
 
-const { body, getPositions } = main();
-const submit = document.createElement('button');
-submit.textContent = 'Submit';
+const { body, getPositions } = Main();
+let controller;
+const submit = document.querySelector('#submitButton');
 submit.addEventListener('click', () => {
   if (getPositions().length === 5) {
-    Controller(getPositions());
+    controller = Controller(getPositions());
+    const allButtons = document.querySelectorAll('button');
+    allButtons.forEach(button => button.classList.add('invisible'));
+    const aiGrid = document.querySelector('#aiGrid');
+    aiGrid.classList.remove('invisible');
   }
 });
-body.append(submit);
+const opponentBoard = document.querySelectorAll('#aiGrid>div');
+opponentBoard.forEach(tile => tile.addEventListener('click', attackHandler))
+function attackHandler (e) {
+    const result = controller.playRound(e.target.dataset.index);
+    if (result[0]) {
+      e.target.classList.add('hit');
+    }
+    else {
+      e.target.classList.add('miss');
+    }
+    const opponentTarget = document.querySelector(`#grid>div[data-index="${result[1]}"]`);
+    if (opponentTarget.classList.contains('ship')) {
+      opponentTarget.classList.add('hit');
+    }
+    else {
+      opponentTarget.classList.add('miss');
+    }
+    if (result[2]) {
+      console.log('GAMEOVER');
+      opponentBoard.forEach(tile => tile.removeEventListener('click', attackHandler));
+    }
+    e.target.removeEventListener('click', attackHandler);
+}
